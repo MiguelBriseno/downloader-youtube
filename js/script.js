e66b2219bdbc329c87926fdc4a7695e65bc51d7b
Miguel Briseño <miguel.briseno.bustos@gmail.com>
2023-01-06 07:01:21 -0600

    Agregamos el evento click al boton asi como la funcion de envio de formulario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict'
+
+var btn = document.querySelector("#send")
+var input = document.querySelector("#input")
+
+btn.addEventListener('click', () => {
+    console.log(`URL: ${input.value}`)
+
+})
+
+function sendURL(){
+    
+}
